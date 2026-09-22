31426716b79c39beb879e5a16f7caef4fee96971
Ranjiniv1414 <ranjini1234ran@gmail.com>
2026-09-22 10:46:16 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,4 @@
-
-          from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uuid, os, json, re
